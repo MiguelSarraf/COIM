672379357d53e692b4adbc86808725b0d92059d6
Miguel Sarraf <miguel.sarraf@alumni.usp.br>
2025-12-29 10:20:14 -0300

    Adjusting base constraints module

--- src/COIM/constraints/constrain.py
+++ src/COIM/constraints/constrain.py
@@ -1,42 +1,71 @@
+"""Base class for definition of a constraint."""
+
+
 class Constrain:
-	"""
-	General class to store the rules.
-	"""
-	def __init__(self, variables, params, labels=None, precision=1e-10):
-		"""
-		(list, list, list, float)->None
-		Save general parameters of the rule
-		"""
-		self.variables=variables
-		self.params=params
-		self.precision=precision
+    """General class to store the rules."""
 
-	def validate_dataframe(self, df):
-		"""
-		(DataFrame)->DataFrame
-		Check if the rule is attended by the dataframe.
-		Must return the complete dataframe.
-		"""
-		return df
+    def __init__(self, variables, params, labels=None, precision=1e-10):
+        """
+        Save general parameters of the rule.
 
-	def format_rule(self):
-		"""
-		(None)->str
-		Returns a string describing the rule.
-		"""
-		return "Description"
+        Args:
+            variables (list): variables used in the contraint
+            params (list): parameters which define the contraint
+            labels (list): names for the new columns created
+            precision (list): precision to be used in DataFrame validation
+        """
+        self.variables = variables
+        self.params = params
+        self.labels = labels
+        self.precision = precision
 
-	def encode_dataframe(self, df):
-		"""
-		(DataFrame)->DataFrame
-		Apply the developed formulas to reduce the dataframe columns.
-		"""
-		return df
+    def validate_dataframe(self, df):
+        """
+        Check if the rule is attended by the dataframe.
 
-	def decode_dataframe(self, df, errors):
-		"""
-		(DataFrame, DataFrame)->DataFrame, DataFrame
-		Apply the reverse formulas to restore the original columns
-		and propagate the errors.
-		"""
-		return df, errors
+        Must return the complete dataframe.
+        If there are any inconsistencies, must raise an error.
+
+        Args:
+            df (pd.DataFrame): The input DataFrame to be validated
+
+        Returns:
+            df (pd.DataFrame): The input DataFrame unmodified
+        """
+        return df
+
+    def format_rule(self):
+        """
+        Return a string describing the rule.
+
+        Returns:
+            rule (str): The string with the mathematical expression for the contraint
+        """
+        rule = "Description"
+        return rule
+
+    def encode_dataframe(self, df):
+        """
+        Apply the developed formulas to reduce the dataframe columns.
+
+        Args:
+            df (pd.DataFrame): The input DataFrame to be encoded
+
+        Returns:
+            df (pd.DataFrame): The encoded DataFrame
+        """
+        return df
+
+    def decode_dataframe(self, df, errors):
+        """
+        Apply the reverse formulas to restore the original columns and propagate the errors.
+
+        Args:
+            df (pd.DataFrame): Encoded values outputed from the inferential model
+            errors (pd.DataFrame): Errors for each encoded field
+
+        Returns:
+            df (pd.DataFrame): Decoded values, true outputs from the inferential model
+            errors (pd.DataFrame): Errors for each decoded true field
+        """
+        return df, errors
